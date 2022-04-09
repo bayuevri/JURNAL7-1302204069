9110857d17cfd7a857c984ea33f4ecd2173c5972
bayuevri <bayuevri@student.telkomuniversity.ac.id>
2022-04-09 17:18:47 +0700

    Add project files.

---
+++ JURNAL7-1302204069/BankTransferConfig.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace JURNAL7_1302204069
+{
+	internal class BankTransferConfig
+	{
+			public string Language { get; set; }
+
+			public int TransferThreshold { get; set; }
+			public int TransferLowFee { get; set; }
+			public int TransferHighFee { get; set; }
+
+			public List<string> Methods { get; set; }
+
+			public string Confirmation { get; set; }
+
+
+			// Menyetting nilai property saat instance dibuat
+			public BankTransferConfig()
+			{
+				Methods = new List<string>();
+				ReadFile();
+			}
+
+			// Melakukan user input
+			public void TryTransfer()
+			{
+				if (Language == "en")
+				{
+					Console.WriteLine("Please insert the amount of money to transfer");
+				}
+				else
+				{
+					Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+				}
+				string rawTransfer = Console.ReadLine();
+				int transfer = int.Parse(rawTransfer);
+				int biayaTransfer;
+
+				int totalBiaya;
+
+				if (transfer <= TransferThreshold)
+				{
+					biayaTransfer = TransferLowFee;
+				}
+				else
+				{
+					biayaTransfer = TransferHighFee;
+				}
+
+				totalBiaya = transfer + biayaTransfer;
+
+				if (Language == "en")
+				{
+					Console.WriteLine($"Transfer fee: {biayaTransfer}");
+					Console.WriteLine($"Total amount: {totalBiaya}");
+
+					Console.WriteLine("\nSelect transfer method: ");
+				}
+				else
+				{
+					Console.WriteLine($"Biaya transfer: {biayaTransfer}");
+					Console.WriteLine($"Total biaya: {totalBiaya}");
+
+					Console.WriteLine("\nPilih metode transfer: ");
+				}
+
+				for (int i = 0; i < Methods.Count; i++)
+				{
+					Console.WriteLine($"{i + 1}. {Methods[i]}");
+				}
+				string method = Console.ReadLine();
+
+				if (Language == "en")
+				{
+					Console.WriteLine($"Please type {Confirmation}");
+				}
+				else
+				{
+					Console.WriteLine($"Ketik {Confirmation}");
+				}
+				string confirm = Console.ReadLine();
+
+				if (confirm == Confirmation)
+				{
+					if (Language == "en")
+					{
+						Console.WriteLine("The transfer is completed");
+					}
+					else
+					{
+						Console.WriteLine("Proses transfer berhasil");
+					}
+				}
+				else
+				{
+					if (Language == "en")
+					{
+						Console.WriteLine("Transfer is cancelled");
+					}
+					else
+					{
+						Console.WriteLine("Transfer dibatalkan");
+					}
+				}
+
+			}
+
+			// Mendapatkan path dari file
+			private string GetFilePath => Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "bank_transfer_config.json");
+
+			// Membaca file config json
+			public void ReadFile()
+			{
+				var file = File.OpenText(GetFilePath);
+
+				JsonElement json = JsonSerializer.Deserialize<JsonElement>(file.ReadToEnd());
+
+				Language = json.GetProperty("lang").GetString();
+
+				TransferThreshold = json.GetProperty("transfer").GetProperty("threshold").GetInt32();
+				TransferLowFee = json.GetProperty("transfer").GetProperty("low_fee").GetInt32();
+				TransferHighFee = json.GetProperty("transfer").GetProperty("high_fee").GetInt32();
+
+				Confirmation = json.GetProperty("confirmation").GetProperty(Language).GetString();
+
+				foreach (var item in json.GetProperty("methods").EnumerateArray().ToList())
+				{
+					Methods.Add(item.GetString());
+				}
+
+			}
+
+		}
+	}
---
+++ JURNAL7-1302204069/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Demo
+using JURNAL7_1302204069;
+
+BankTransferConfig config = new BankTransferConfig();
+config.TryTransfer();
